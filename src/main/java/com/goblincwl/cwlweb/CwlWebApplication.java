788880bd7a12b0baf4eefa0abc8cd1afcf48d764
GoblinCwl <qwaszx123>
2021-04-23 11:02:35 +0800

    <初始化>

---
+++ src/main/java/com/goblincwl/cwlweb/CwlWebApplication.java
@@ -0,0 +1,13 @@
+package com.goblincwl.cwlweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwlWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwlWebApplication.class, args);
+    }
+
+}
