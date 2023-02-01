c2fa9da2ad716231c0e1916c5586701fa00ab163
chenweilong <12345678>
2023-02-01 20:12:35 +0800

    feat: sql

--- .WorkSpace/MySqlScript/Tables.sql
+++ .WorkSpace/MySqlScript/Tables.sql
@@ -1,4 +1,11 @@
-create database `cwl-web`;
+create table if not exists `cwl-web`.access_log
+(
+    id               int auto_increment
+        primary key,
+    access_record_id int                                not null,
+    access_date      datetime default CURRENT_TIMESTAMP not null
+)
+    charset = utf8mb4;
 
 create table if not exists `cwl-web`.access_record
 (
@@ -10,6 +17,21 @@ create table if not exists `cwl-web`.access_record
     last_access_time   datetime     null,
     access_count       int          null,
     last_watering_time datetime     null
+)
+    charset = utf8mb4;
+
+create table if not exists `cwl-web`.app
+(
+    id          int auto_increment
+        primary key,
+    name        varchar(100)       null,
+    html        mediumtext         null,
+    description varchar(300)       null,
+    is_lock     int    default 0   not null,
+    uses_times  bigint default 0   not null,
+    icon_file   varchar(200)       null,
+    color       varchar(100)       null,
+    sort        int    default 999 null
 )
     charset = utf8mb4;
 
@@ -26,13 +48,14 @@ create table if not exists `cwl-web`.blog
 (
     id            int auto_increment
         primary key,
-    title         varchar(100)                       null,
-    content       varchar(5000)                      null,
+    title         text                               null,
+    content       mediumtext                         null,
     release_time  datetime default CURRENT_TIMESTAMP null,
     update_time   datetime default CURRENT_TIMESTAMP null,
     tabs          varchar(500)                       null,
     short_content varchar(200)                       null,
-    do_archive    int(1)   default 0                 null
+    do_archive    int(1)   default 0                 not null,
+    browser_times bigint   default 0                 not null
 )
     charset = utf8mb4;
 
@@ -47,29 +70,41 @@ create table if not exists `cwl-web`.blog_tabs
 )
     charset = utf8mb4;
 
+create table if not exists `cwl-web`.blog_tabs_subscribe
+(
+    id             int auto_increment
+        primary key,
+    email          varchar(100)                       not null,
+    blog_tabs_id   int                                not null,
+    subscribe_time datetime default CURRENT_TIMESTAMP not null
+)
+    charset = utf8mb4;
+
 create table if not exists `cwl-web`.chat_message
 (
     id        int auto_increment
         primary key,
-    content   varchar(200) null,
-    send_time datetime     null
+    content   text     null,
+    send_time datetime null
 )
     charset = utf8mb4;
 
 create table if not exists `cwl-web`.comment
 (
-    id            int auto_increment
+    id                int auto_increment
         primary key,
-    parent_id     int                                null,
-    blog_id       int                                null,
-    nick_name     varchar(200)                       null,
-    profile_url   varchar(200)                       null,
-    content       varchar(5000)                      null,
-    send_time     datetime default CURRENT_TIMESTAMP null,
-    email         varchar(200)                       null,
-    website       varchar(200)                       null,
-    ip_address    varchar(20)                        null,
-    website_audit int(1)   default 0                 null
+    parent_id         int                                null,
+    for_id            int                                null,
+    blog_id           int                                null,
+    nick_name         varchar(200)                       null,
+    profile_url       varchar(1000)                      null,
+    content           text                               null,
+    send_time         datetime default CURRENT_TIMESTAMP null,
+    email             varchar(200)                       null,
+    website           varchar(200)                       null,
+    ip_address        varchar(20)                        null,
+    website_audit     int(1)   default 0                 null,
+    verification_code varchar(64)                        null
 )
     charset = utf8mb4;
 
@@ -77,14 +112,8 @@ create table if not exists `cwl-web`.key_value_options
 (
     opt_key   varchar(200) not null
         primary key,
-    opt_value varchar(200) null,
+    opt_value text         null,
     remark    varchar(200) null
-)
-    charset = utf8mb4;
-
-create table if not exists `cwl-web`.login_body
-(
-    password varchar(200) null
 )
     charset = utf8mb4;
 
---
+++ .WorkSpace/MySqlScript/data/app.sql
@@ -0,0 +1,41 @@
+insert into MY_TABLE (id, name, html, description, is_lock, uses_times, icon_file, color, sort)
+values  (99, 'JRebel激活', '<html lang="zh">
+<body>
+<h1>您好，这里是Jrebel许可证服务器！</h1>
+<p>
+    许可证服务器启动于
+    <a target="_blank" class="url" style="color: #6ecadc;font-weight: bold"></a>
+</p>
+<p>
+    JRebel 7.1及更早版本激活地址为：
+    <span style=''color:red''><span class="url"></span>/{tokenname}</span>
+    , 填写任意电子邮件。
+</p>
+<p>
+    JRebel 2018.1及更高版本激活地址为：
+    <span class="url"></span>/随机生成的UUID<br>
+    (示例(可直接使用):
+    <span style=''color:#3ec45e;font-weight: bold''>
+        <span class="url"></span>/<span class="uuid"></span>
+    </span>
+    ), 填写任意电子邮件。
+</p>
+</body>
+<div>
+    <script src="/webjars/jquery/3.6.1/jquery.min.js"></script>
+    <script src="/webjars/jquery-cookie/1.4.1-1/jquery.cookie.js"></script>
+    <script>
+        const ctx = window.parent.location.protocol + "//" + window.parent.location.host;
+        $(function () {
+            $(".url").text(ctx)
+            $("a.url").attr("href", ctx + "/app/jrebel")
+            $(".uuid").text(''xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx''.replace(/[xy]/g, function (c) {
+                const r = Math.random() * 16 | 0, v = c === ''x'' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            }))
+        })
+    </script>
+</div>
+</html>', '通过本站的许可证服务器，激活你的JRebel/XRebel。', 0, 144, 'appIcon/13ff3bc7-7db7-4d13-bf6c-f7322a3ad2c5.png', '#5fb53b', 1),
+        (100, '邮件模板预览', '', '', 1, 10, 'appIcon/f2cd1741-93e1-416d-be03-8591fc6e33a1.png', '#24710e', 2),
+        (101, '1', '', '', 0, 2, 'appIcon/ab5af727-6615-4e44-84f7-48d42c2c1cb5.png', '#000000', 2);
